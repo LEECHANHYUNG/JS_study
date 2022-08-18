46cdfb3a09ccfa043dd5b3704437ae5f1308d089
leechanhyung <lsj721538@gmail.com>
2022-08-18 17:38:28 +0900

    2022-08-18

---
+++ CH09/main.js
@@ -0,0 +1,16 @@
+var x = 10;
+
+var str = x.toString();
+console.log(typeof str, str);
+
+console.log(1 / "2");
+
+console.log("Cat" && "Dog");
+console.log("Cat" || "Dog");
+console.log(false && anything);
+
+/* var elem = null;
+var value = elem.value; */
+var elem = null;
+var value = elem && elem.value;
+console.log(value);
